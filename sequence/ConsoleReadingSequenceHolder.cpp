47ed1c21b3c06f991614b93b5b8b61a0ebc3784b
VladislavArzamastsev <68342736+VladislavArzamastsev@users.noreply.github.com>
2021-05-14 14:41:11 +0300

    Created ConsoleReadingSequenceHolder

---
+++ sequence/ConsoleReadingSequenceHolder.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Vladislav on 14.05.2021.
+//
+
+#include <iostream>
+#include "ConsoleReadingSequenceHolder.h"
+
+void ConsoleReadingSequenceHolder::generateRandomSequence() {
+    int lastNumber = -1;
+    std::cout<<"Input your numbers. To exit, input 0"<<std::endl;
+    while(lastNumber != 0){
+        std::cin >> lastNumber;
+        if(lastNumber > this->getMax()){
+            this->max = lastNumber;
+        }else if(lastNumber < this->getMin()){
+            this->min = lastNumber;
+        }
+        this->data.push_back(lastNumber);
+    }
+    std::cout<<"Max: "<<this->getMax()<<std::endl;
+    std::cout<<"Min: "<<this->getMin()<<std::endl;
+    std::cout<<"Avg: "<<this->getAverage()<<std::endl;
+}
---
+++ sequence/ConsoleReadingSequenceHolder.h
@@ -0,0 +1,18 @@
+//
+// Created by Vladislav on 14.05.2021.
+//
+
+#ifndef OOPMOD_CONSOLEREADINGSEQUENCEHOLDER_H
+#define OOPMOD_CONSOLEREADINGSEQUENCEHOLDER_H
+
+
+#include "SequenceHolder.h"
+
+class ConsoleReadingSequenceHolder : public SequenceHolder {
+
+    void generateRandomSequence() override;
+
+};
+
+
+#endif //OOPMOD_CONSOLEREADINGSEQUENCEHOLDER_H
