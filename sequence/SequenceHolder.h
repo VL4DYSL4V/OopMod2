04f698b27bbf2d11961e82b95ca47406839f756e
VladislavArzamastsev <68342736+VladislavArzamastsev@users.noreply.github.com>
2021-05-14 14:21:55 +0300

    Project setup

---
+++ sequence/SequenceHolder.h
@@ -0,0 +1,16 @@
+//
+// Created by Vladislav on 14.05.2021.
+//
+
+#ifndef OOPMOD_SEQUENCEHOLDER_H
+#define OOPMOD_SEQUENCEHOLDER_H
+
+
+class SequenceHolder {
+
+
+
+};
+
+
+#endif //OOPMOD_SEQUENCEHOLDER_H
---
+++ test/tests.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Vladislav on 14.05.2021.
+//
+
